8378c9a23b5d63eb00f7c3ea02f947a8b7fc6289
bella-ans <bella.ans@kakaocorp.com>
2019-04-13 00:22:19 +0900

    2xn 타일링

---
+++ 11726_dp/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int[] cases = new int[num + 1];
+
+        cases[0] = 1;
+        cases[1] = 1;
+
+        for (int i = 2; i < num + 1; i++) {
+            cases[i] = (cases[i - 1] + cases[i - 2]) % 10007;
+        }
+
+        System.out.println(cases[num]);
+    }
+}
